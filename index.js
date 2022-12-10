e2af57a9ef16d25086ae7d224b345e80e7134de8
Nate Cater <ncater3@gatech.edu>
2022-12-10 15:52:22 -0800

    added ability to change grid size, which replaces cells in DOM

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const container = document.querySelector(".container");
+const clearBtn = document.querySelector("#clear");
+const updateBtn = document.querySelector("#change");
+
+function paintItBlack(e) {
+    e.target.style.setProperty('background-color', 'black');
+};
 
 function createGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
@@ -6,6 +12,7 @@ function createGrid(rows, cols) {
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = "cell";
+        cell.addEventListener('mouseover', paintItBlack);
     };
 };
 
@@ -13,14 +20,45 @@ createGrid(16, 16);
 
 let sketchCells = document.getElementsByClassName('cell');
 
-function paintItBlack(e) {
-    e.target.style.setProperty('background-color', 'black');
+function clearGrid() {
+    for (let i = 0; i < sketchCells.length; i++) {
+        sketchCells[i].style.setProperty('background-color', 'white');
+    };
 };
 
+function updateGrid() {
+    let input = 16;
+
+    do {
+        input = parseInt(prompt("Please enter the number of rows/columns you want (no more than 100): "));
+
+        if (!Number.isInteger(input) || input < 1 || input > 100) {
+            alert("Invalid input! Enter a number between 1 and 100");
+        }
+    } while (!Number.isInteger(input) || input < 1 || input > 100);
+
+    deleteCells();
+
+    createGrid(input, input);
+
+}
+
+function deleteCells() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.remove();
+    });
+}
+
+updateBtn.addEventListener('click', updateGrid);
+clearBtn.addEventListener('click', clearGrid);
+
+
+/*
 for (let i = 0; i < sketchCells.length; i++) {
     sketchCells[i].addEventListener('mouseover', paintItBlack);
 };
-
+*/
 
 
 
